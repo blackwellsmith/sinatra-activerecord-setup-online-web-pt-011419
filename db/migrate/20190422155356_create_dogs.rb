0ab0847f4795360754f98631123014e5a94cdc10
blackwell <blackwellsmithiv@gmail.com>
2019-04-22 15:59:32 +0000

    Automatically backed up by Learn

--- db/migrate/20190422155356_create_dogs.rb
+++ db/migrate/20190422155356_create_dogs.rb
@@ -1,9 +1,10 @@
-class CreateDogs < ActiveRecord::Migration[4.0]
+class CreateDogs < ActiveRecord::Migration
   def up
     create_table :dogs do |t|
       t.string :name 
       t.string :breed
   end
+end
   
   def down
     drop_table :dogs
